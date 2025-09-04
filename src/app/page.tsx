d562a067c7f2fcc81441a7805cf62cf4a11c2241
서유림 <yurim2204@nsonesoft.com>
2025-09-04 17:04:16 +0900

    feat: 색상 변수 정의

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div className="text-xl bg-red-300">test</div>;
+  return (
+    <div className="flex flex-col gap-16 h-screen">
+      <div className="p-10 bg-header-bg">
+        <p className="text-3xl font-extrabold text-point-color">Cine Pick</p>
+      </div>
+      <p className="p-4 bg-text-bg w-fit text-xl">test</p>
+      <div className="p-16 bg-header-bg" />
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,22 @@ const config = {
       fontFamily: {
         sans: ["Pretendard Variable", "sans-serif"],
       },
+      colors: {
+        white: "hsl(var(--white))",
+        black: "hsl(var(--black))",
+        gray: {
+          100: "hsl(var(--gray-100))",
+          700: "hsl(var(--gray-700))",
+          800: "hsl(var(--gray-800))",
+        },
+        red: "hsl(var(--red-100))",
+
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        "text-bg": "hsl(var(--text-bg))",
+        "header-bg": "hsl(var(--header-bg))",
+        "point-color": "hsl(var(--point-color))",
+      },
     },
   },
   plugins: [],
