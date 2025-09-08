84e2163cdd53009eedf1231a0c690d78d23b7b5f
서유림 <yurim2204@nsonesoft.com>
2025-09-08 10:47:28 +0900

    feat: 영화리스트 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import PopularSectionComponent from "./components/popular-section.component";
+"use client";
+
+import { useMovies } from "@/queries/movie.query";
+import MovieListComponent from "@/components/movie-list/movie-list.component";
 
 export default function Home() {
+  const { data, isLoading, isError } = useMovies({
+    sort_by: "popularity.desc",
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error loading movies</p>;
+
   return (
-    <>
-      <PopularSectionComponent />
-    </>
+    <main className="flex flex-col gap-16 pb-40">
+      <MovieListComponent
+        title="오늘의 인기 영화"
+        bgColor="hsl(var(--point-color))"
+        data={data.results}
+      />
+      <MovieListComponent title="최신 개봉작" data={data.results} />
+      <MovieListComponent title="장르별 추천 영화" data={data.results} />
+    </main>
   );
 }
---
+++ src/components/movie-list/movie-list.component.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { MovieItem } from "@/types/movie.type";
+import ButtonComponent from "../button/button.component";
+import MovieCardComponent from "../movie-card/movie-card.component";
+
+interface MovieListProps {
+  title: string;
+  bgColor?: string;
+  data: MovieItem[];
+}
+
+export default function MovieListComponent({ title, bgColor, data }: MovieListProps) {
+  return (
+    <section style={{ backgroundColor: bgColor }} className="flex p-6">
+      <div className="mx-auto flex w-full max-w-[1920px] flex-col justify-center gap-4">
+        <div className="flex items-center justify-between text-nowrap">
+          <h2 className="text-3xl font-bold">{title}</h2>
+          <ButtonComponent>전체보기</ButtonComponent>
+        </div>
+
+        <div className="flex items-center overflow-x-auto">
+          <div className="mb-4 flex w-full items-center gap-4 px-4">
+            {data.map((item: MovieItem) => (
+              <MovieCardComponent key={item.id} data={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
