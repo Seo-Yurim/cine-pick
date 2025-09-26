24dfca40737794a6f823b89d8fd454d6e14e3946
서유림 <yurim2204@naver.com>
2025-09-26 15:59:56 +0900

    feat: 정렬 추가

--- src/app/movies/components/movies-header.component.tsx
+++ src/app/movies/components/movies-header.component.tsx
@@ -4,6 +4,8 @@ import { DateValue } from "react-aria-components";
 import { CiBoxList, CiGrid41 } from "react-icons/ci";
 import { RiFilterFill } from "react-icons/ri";
 import { MovieParams } from "@/types/movie.type";
+import { useModalStore } from "@/stores/modal.store";
+import { useGenres } from "@/queries/movie.query";
 import {
   ButtonComponent,
   CheckboxComponent,
@@ -12,7 +14,16 @@ import {
   ToggleButtonComponent,
 } from "@/components";
 import { DatePickerComponent } from "@/components/date-picker/date-picker.component";
-import { useGenres } from "@/queries/movie.query";
+
+// constants
+const sortOptions = [
+  { label: "인기 내림차순", value: "popularity.desc" },
+  { label: "인기 오름차순", value: "popularity.asc" },
+  { label: "출시일 내림차순", value: "primary_release_date.desc" },
+  { label: "출시일 오름차순", value: "primary_release_date.asc" },
+  { label: "제목 내림차순", value: "title.desc" },
+  { label: "제목 오름차순", value: "title.asc" },
+];
 
 const toggleMenus = [
   { label: <CiGrid41 className="h-8 w-8" />, value: "grid" },
@@ -24,16 +35,15 @@ export type DatePickerType = {
   end: DateValue | null;
 };
 
-export default function MoviesHeaderComponent({
-  tab,
-  onTab,
-  onParams,
-}: {
+interface MoviesHeaderProps {
   tab: string;
   onTab: React.Dispatch<React.SetStateAction<string>>;
   onParams: React.Dispatch<React.SetStateAction<MovieParams>>;
-}) {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+}
+
+export default function MoviesHeaderComponent({ tab, onTab, onParams }: MoviesHeaderProps) {
+  const { modals, toggleModal } = useModalStore();
+  const [sortOption, setSortOption] = useState<{ label: string; value: string }>(sortOptions[0]);
   const [genreSelected, setGenreSelected] = useState<string[]>([]);
   const [selectedDate, setSelectedDate] = useState<DatePickerType>({
     start: null,
@@ -47,7 +57,7 @@ export default function MoviesHeaderComponent({
     const personIds = await getPersonIds(tagList);
 
     onParams({
-      sort_by: "popularity.desc",
+      sort_by: sortOption.value,
       with_genres: genreSelected.toString(),
       with_people: personIds.toString(),
       "primary_release_date.gte": selectedDate.start?.toString(),
@@ -65,17 +75,44 @@ export default function MoviesHeaderComponent({
           activeTab={tab}
           onChange={(tab) => onTab(tab)}
         />
+
+        <div className="relative flex flex-col items-center text-nowrap rounded-xl border p-1">
+          <ButtonComponent onClick={() => toggleModal("sortMenu")}>
+            {sortOption.label}
+          </ButtonComponent>
+
+          {modals.sortMenu && (
+            <div className="absolute top-full z-50 mt-2 flex flex-col gap-2 rounded-xl border bg-black p-4 text-lg">
+              {sortOptions.map((option) => (
+                <ButtonComponent
+                  key={option.value}
+                  onClick={() => {
+                    setSortOption(option);
+                    onParams((prev) => ({
+                      ...prev,
+                      sort_by: option.value,
+                    }));
+                  }}
+                  className={`${sortOption === option && "text-point-color"} hover:bg-white/50`}
+                >
+                  {option.label}
+                </ButtonComponent>
+              ))}
+            </div>
+          )}
+        </div>
+
         <div
-          onClick={() => setIsOpen(!isOpen)}
-          className={`${isOpen && "bg-point-color"} cursor-pointer rounded-xl border p-2 transition-colors duration-300 hover:bg-point-color`}
+          onClick={() => toggleModal("filterMenu")}
+          className={`${modals.filterMenu && "bg-point-color"} cursor-pointer rounded-xl border p-2 transition-colors duration-300 hover:bg-point-color`}
         >
           <RiFilterFill className="h-8 w-8" />
         </div>
       </div>
 
-      {isOpen && (
+      {modals.filterMenu && (
         <div
-          className={`flex flex-col items-center rounded-xl bg-point-color p-4 ${isOpen ? "scale-100 animate-slide-down opacity-100" : "pointer-events-none scale-95 opacity-0"}`}
+          className={`flex flex-col items-center rounded-xl bg-point-color p-4 ${modals.filterMenu ? "scale-100 animate-slide-down opacity-100" : "pointer-events-none scale-95 opacity-0"}`}
         >
           <div className={`grid grid-cols-2 gap-4`}>
             <div className="flex flex-col">
