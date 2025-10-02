50fe4ff3c1a56df6918894708d8871a1728fdcd1
서유림 <yurim2204@naver.com>
2025-10-02 12:21:27 +0900

    fix: CTA 버튼 컴포넌트 변경

--- src/app/movies/_components/scroll-to-top.component.tsx
+++ src/app/movies/_components/scroll-to-top.component.tsx
@@ -1,16 +1,17 @@
+import { Button } from "react-aria-components";
 import { FaArrowAltCircleUp } from "react-icons/fa";
-import { ButtonComponent } from "@/components";
 
 export function ScrollToTop() {
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
   return (
-    <ButtonComponent
+    <Button
       onClick={scrollToTop}
       className="fixed bottom-6 right-6 rounded-full bg-point-color p-3"
     >
       <FaArrowAltCircleUp className="h-12 w-12 text-white" />
-    </ButtonComponent>
+    </Button>
   );
 }
