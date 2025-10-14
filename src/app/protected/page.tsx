625b7dc6db5b2076a150fb9b40834ac3ed02866b
서유림 <yurim2204@naver.com>
2025-10-14 10:12:10 +0900

    feat: 접근 불가 페이지 수정

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import { IoIosWarning } from "react-icons/io";
+
 export default function ProtectedPage() {
-  return <div>접근 불가한 페이지 입니다. 로그인해주세요.</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-8 py-40">
+      <IoIosWarning size={144} className="text-yellow-400" />
+      <p className="text-3xl font-bold">접근 불가한 페이지 입니다.</p>
+    </div>
+  );
 }
